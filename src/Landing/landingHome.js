6f2386a46eeacb5567d7ce5e319d5d03afe06f07
Darren <darren.hu3@gmail.com>
2021-01-05 17:19:27 -0500

    update app component

---
+++ src/Landing/landingHome.js
@@ -0,0 +1,11 @@
+import React from "react"
+import SearchHome from "../Search/searchHome"
+
+
+function LandingHome() {
+    return <div>
+        <SearchHome/>
+    </div>
+}
+
+export default LandingHome
