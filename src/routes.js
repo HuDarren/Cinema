5b175072eba00857ec6b3a72b031660996212d58
Darren <darren.hu3@gmail.com>
2021-01-20 14:47:44 -0500

    add blog routes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { withRouter, Route, Switch } from 'react-router-dom';
-import LandingHome from "./Landing/landingHome"
-import Blog from "./Blog/blogHome"
+import React from "react";
+import { withRouter, Route, Switch } from "react-router-dom";
+import LandingHome from "./Landing/landingHome";
+import Blog from "./Blog/blog";
+import Database from "./Blog/blog1";
+import Network from "./Blog/blog2";
+import ReactHook from "./Blog/blog3";
+import RestApi from "./Blog/blog4";
 
 function Routes() {
   return (
-    <div className="wrapper">
-      <h1>Marine Mammals</h1>
-        <Switch>
-      <Route exact path="/home" component={LandingHome} />
-        <Route exact path="/view" component={Blog} />
-        </Switch>
+    <div className="">
+      <Switch>
+        <Route exact path="/home" component={LandingHome} />
+        <Route exact path="/blog/database" component={Database} />
+        <Route exact path="/blog/networks" component={Network} />
+        <Route exact path="/blog/reactHooks" component={ReactHook} />
+        <Route exact path="/blog/restAPI" component={RestApi} />
+      </Switch>
     </div>
   );
 }
