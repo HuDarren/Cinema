c750e3c87c944d6f3ef938dcc34922f8c8d7af66
Darren <darren.hu3@gmail.com>
2021-01-20 14:48:50 -0500

    add footer component

---
+++ src/Footer/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-content">
+        <span className="footer-text"> &copy; 2021 Darren Hu</span>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
