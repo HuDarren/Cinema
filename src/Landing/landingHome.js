9ffedbd212cb38a303c5319f94ba6dfdd8889e39
Darren <darren.hu3@gmail.com>
2021-01-20 14:49:53 -0500

    change home page

--- src/Landing/landingHome.js
+++ src/Landing/landingHome.js
@@ -1,11 +1,20 @@
-import React from "react"
-import SearchHome from "../Search/searchHome"
-
+import React from "react";
+import About from "../About/about";
+import Project from "../Project/project";
+import Blog from "../Blog/blog";
+import Contact from "../Contact/contact";
 
 function LandingHome() {
-    return <div>
-        <SearchHome/>
+  return (
+    <div className="landingHome">
+      <div>
+        <About />
+        <Project />
+        <Blog />
+        <Contact />
+      </div>
     </div>
+  );
 }
 
-export default LandingHome
+export default LandingHome;
