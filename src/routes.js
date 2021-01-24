2b15aa191f381cd7d9db9b3ed0572ff5e580e672
Darren <darren.hu3@gmail.com>
2021-01-24 16:54:14 -0500

    add hashrouter

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withRouter, Route, Switch } from "react-router-dom";
+import { withRouter, Route, Switch, HashRouter } from "react-router-dom";
 import LandingHome from "./Landing/landingHome";
 import Database from "./Blog/blog1";
 import Network from "./Blog/blog2";
@@ -8,15 +8,15 @@ import RestApi from "./Blog/blog4";
 
 function Routes() {
   return (
-    <div className="">
+    <HashRouter>
       <Switch>
-        <Route component={LandingHome} />
+        <Route exact path="/" component={LandingHome} />
         <Route exact path="/blog/database" component={Database} />
         <Route exact path="/blog/networks" component={Network} />
         <Route exact path="/blog/reactHooks" component={ReactHook} />
         <Route exact path="/blog/restAPI" component={RestApi} />
       </Switch>
-    </div>
+    </HashRouter>
   );
 }
 
