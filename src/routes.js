8cfcd1cd2ac9162e84d945504dbd865196551211
Darren <darren.hu3@gmail.com>
2021-01-24 17:16:33 -0500

    update file name

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import RestApi from "./Blog/blog4";
 
 function Routes() {
   return (
-    <HashRouter basename="/hudarren">
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Route exact path="/" component={LandingHome} />
       <Route exact path="/blog/database" component={Database} />
       <Route exact path="/blog/networks" component={Network} />
